050959684d0b9e1782ab429f15567653b0fb0cd4
Your Name <you@example.com>
2023-05-31 12:11:58 -0400

    initial commit

---
+++ estore/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg'
+import './App.css'
+import TopNav from './components/TopNav'
+import CatNav from './components/CatNav'
+import MainComponent from './components/MainComponent'
+
+function App() {
+  return (
+    <div className='App'>
+     <TopNav/>
+     <CatNav/>
+     <MainComponent/>
+    </div>
+  )
+}
+
+export default App
